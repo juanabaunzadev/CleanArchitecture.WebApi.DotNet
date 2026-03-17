2906c168d1c5b096b541ec8c9956a96677821ea4
Juan Abaunza <jabaunzagarcia@gmail.com>
2026-03-16 22:28:06 -0600

    chore(presentation): add presentation layer project

---
+++ CleanArchitecture.WebApi.Presentation/Program.cs
@@ -0,0 +1,30 @@
+using CleanArchitecture.WebApi.Application;
+using CleanArchitecture.WebApi.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
